eb683069561681ae85f38eac895c0c538e441fc9
leesooim <soojeng22@outlook.com>
2025-02-09 14:15:25 +0900

    feat(Todo) : Todo List 구현 (#5)

--- TodoFE/todo-react-app/src/App.js
+++ TodoFE/todo-react-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import  Todo from './pages/todo/Todo'
+import { List, Paper } from "@mui/material";
 
 function App() {
+  const [item,setItem] = useState([
+    {
+    id:"0",
+    title:"Hello World",
+    done:true
+  },
+  {
+    id:"1",
+    title:"Hello World2",
+    done:false
+  }
+])
+
+  let todoItems = item.length > 0 && (
+    <Paper style={{margin:16}}>
+      <List>
+        {item.map((item) =>(
+          <Todo item={item} key={item.id}/>
+         ))}
+      </List>
+    </Paper>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todoItems}
     </div>
   );
 }
